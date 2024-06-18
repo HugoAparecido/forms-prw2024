df254255b195063a80b4e22c646ebcbbca41fe4e
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2024-06-18 15:06:27 -0300

    18/06 ex5

---
+++ exercicio_18_06/ex5/form.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $largura = intval($_GET['largura']);
+    $comprimento = intval($_GET['comprimento']);
+    echo "<h3>A área do terreno é <span style='color: green'>" . number_format((floatval($largura) * floatval($comprimento)), 2, ",", ".") . "</span>m<sup>2</sup></h3>";
+    ?>
+</body>
+
+</html>
